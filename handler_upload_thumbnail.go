156df74bd3519355182099d6c8f2112b1461d41b
alaw22 <lawsonalex1999@gmail.com>
2025-09-01 14:14:02 -0500

    Feature: embedded files inline using base64 encoding

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io"
+	"encoding/base64"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -32,6 +34,83 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	// "thumbnail" should match the HTML form input name
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType := header.Header.Get("Content-Type")
+	if mediaType == "" {
+		respondWithError(w, http.StatusBadRequest, "Content-Type header is empty", err)
+		return
+	}
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to read content bytes", err)
+		return		
+	}
+
+	// Encode thumbnail
+	base64Data := base64.StdEncoding.EncodeToString(data)
+
+	// Create data URL
+	dataURL := fmt.Sprintf("data:%s;base64,%s",mediaType,base64Data)
+
+	// Get video meta-data from video ID
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, 500, "Video doesn't exist", err)
+		return				
+	}
+
+	if video.CreateVideoParams.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "User is not the owner of video", err)
+		return				
+	}
+
+	// Save thumbnail in global map
+	// videoThumbnails[videoID] = thumbnail{
+	// 	data: data,
+	// 	mediaType: mediaType,
+	// }
+
+	// thumbnail url
+	// thumbnailURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s",cfg.port,videoIDString)
+
+	video.ThumbnailURL = &dataURL
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, 500, "Unable to update video meta data", err)
+		return						
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
+
 }
+
+
+/* type Video struct {
+	ID           uuid.UUID `json:"id"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+	ThumbnailURL *string   `json:"thumbnail_url"`
+	VideoURL     *string   `json:"video_url"`
+	CreateVideoParams
+} 
+	
+type thumbnail struct {
+	data      []byte
+	mediaType string
+}
+
+var videoThumbnails = map[uuid.UUID]thumbnail{}
+
+*/
